f6bcef000836e15cf4eefe7300856859d780c560
simply10w <petar.djordjevic94@outlook.com>
2019-06-26 01:04:16 +0200

    chore: setup core module, setup editor settings, install angular modules

--- apps/slagalica/src/app/app.component.ts
+++ apps/slagalica/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'slagalica-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'slagalica';
+export class AppComponent implements OnInit {
+  title$: Observable<string> = this.http
+    .get<any>('/api')
+    .pipe(map(response => response.message));
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
 }
--- apps/slagalica/src/app/app.module.ts
+++ apps/slagalica/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
