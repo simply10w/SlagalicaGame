704bcd129d7f3ed7c02ebd5e311765fc2d10431f
simply10w <petar.djordjevic94@outlook.com>
2019-07-06 10:54:34 +0200

    asocijacija game adjustments

--- apps/slagalica/src/app/player/containers/asocijacija-game/asocijacija-game.component.ts
+++ apps/slagalica/src/app/player/containers/asocijacija-game/asocijacija-game.component.ts
@@ -1,16 +1,13 @@
 import {
   ChangeDetectionStrategy,
-  Component,
   ChangeDetectorRef,
-  OnDestroy,
-  OnInit
+  Component
 } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import * as fromPlayer from '@slagalica-app/player/reducers';
-import { map, withLatestFrom, tap } from 'rxjs/operators';
-import { PlayerRole, AsocijacijaStates } from '@slagalica/data';
-import { times } from 'lodash';
 import { AsocijacijaGameActions } from '@slagalica-app/player/actions';
+import { AsocijacijaStates } from '@slagalica/data';
+import { times } from 'lodash';
+import { selectors } from './asocijacija.selectors';
 
 @Component({
   selector: 'sla-asocijacija-game',
@@ -19,56 +16,28 @@ import { AsocijacijaGameActions } from '@slagalica-app/player/actions';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AsocijacijaGameComponent {
-  amIRed$ = this.store.pipe(select(fromPlayer.getAmIRed));
-  amIBlue$ = this.store.pipe(select(fromPlayer.getAmIBlue));
-
-  game$ = this.store
-    .pipe(select(fromPlayer.getAsocijacijaGame))
-    .pipe(tap(() => this.cd.markForCheck()));
-
   AsocijacijaStates = AsocijacijaStates;
 
-  turn$ = this.game$.pipe(map(game => game.state));
-
-  turnColor$ = this.turn$.pipe(
-    map(turn => {
-      return {
-        red:
-          turn === AsocijacijaStates.RedPlaying ||
-          turn === AsocijacijaStates.RedSolving,
-        blue:
-          turn === AsocijacijaStates.BluePlaying ||
-          turn === AsocijacijaStates.BlueSolving
-      };
-    })
+  currentStateDescription$ = this.store.pipe(
+    select(selectors.getCurrentStateDescription)
   );
+  turnColor$ = this.store.pipe(select(selectors.getTurnColor));
 
-  disabledInputs$ = this.turn$.pipe(
-    withLatestFrom(this.amIRed$, this.amIBlue$),
-    map(([turn, amIRed, amIBlue]) => {
-      if (turn === AsocijacijaStates.RedSolving && amIRed) {
-        return false;
-      } else if (turn === AsocijacijaStates.BlueSolving && amIBlue) {
-        return false;
-      } else {
-        return true;
-      }
-    })
-  );
+  isInputEnabled$ = this.store.pipe(select(selectors.isInputEnabled));
+
+  tilesList$ = this.store.pipe(select(selectors.getTiles));
+  solvedList$ = this.store.pipe(select(selectors.getSolvedList));
+
+  redPlayerPoints$ = this.store.pipe(select(selectors.getRedPlayerPoints));
+  bluePlayerPoints$ = this.store.pipe(select(selectors.getBluePlayerPoints));
+
+  gameSolution$ = this.store.pipe(select(selectors.getGameSolution));
 
   groupSolutions = times(4, () => '');
   gameSolution = '';
 
   GROUPS = times(4);
   HINTS = times(4);
-
-  solved$ = this.game$.pipe(
-    map(game => game.solved),
-    map(solved => {
-      const solutions = [4, 9, 14, 19];
-      return solutions.map(solution => solved[solution]).map(Boolean);
-    })
-  );
 
   constructor(private store: Store<any>, private cd: ChangeDetectorRef) {}
 
@@ -99,5 +68,9 @@ export class AsocijacijaGameComponent {
     );
 
     this.gameSolution = '';
+  }
+
+  changeGameSolution(change: string) {
+    this.gameSolution = change;
   }
 }
